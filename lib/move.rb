43a4b5af5370d0d993d19797e7120be71d17c369
Roshaan Singh <swaggsikh@gmail.com>
2021-03-15 20:07:10 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(array, index, value = "X")
+def move(array, index, value = X)
   board[input_to_index] = value
   return board
 end
